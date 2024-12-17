37f44a519ab220d097a7acd4699a7a7101415e12
isralr <ilaramac@myuax.com>
2024-12-17 11:18:14 +0100

    ejercicio 3, 4 y 5 terminados

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include "ejercicio2/Contenedores_Estándar_y_Secuencias.h"
 #include "ejercicio3/Algoritmos.h"
 #include "ejercicio4/Cálculo_numérico.h"
+#include "ejercicio5/Cmasmas_Moderno.h"
+#include "ejercicio6/Introducción_a_Boost.h"
 
 int main() {
     ejercicio1();
     ejercicio2();
     ejercicio3();
     ejercicio4();
-
+    ejercicio5();
+    ejercicio6();
     return 0;
 }
